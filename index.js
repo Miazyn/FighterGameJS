ec924396bd79731cde4da6828724ff823c7130f1
Miazyn <michelle.ma.fischer@gmail.com>
2024-06-04 18:33:28 +0200

    Added Healthbar UI
    Now you can visbibly see if you are being damaged, no
    consequences as of yet.
    
    Issues not being worked on:
    - Jump too high/away
    - Out of bounds
    - Only attack in one direction
    
    Until further notice, will not be mentioned in commits.

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ class Sprite {
         }
         this.color = color
         this.isAttacking
+        this.health = 100
     }
     draw() {
         ctx.fillStyle = this.color
@@ -157,7 +158,8 @@ function animate() {
         && player.isAttacking)
     {
         player.isAttacking = false
-        console.log('go')
+        enemy.health -= 20
+        document.querySelector('#enemyHealthbar').style.width = enemy.health + '%'
     }
 
 
@@ -167,7 +169,8 @@ function animate() {
     })
         && enemy.isAttacking) {
         enemy.isAttacking = false
-        console.log('enemy attack sucessfull')
+        player.health -= 20
+        document.querySelector('#playerHealthbar').style.width = player.health + '%'
     }
 }
 
