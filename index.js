796f5d946de958bb46186720601909336f184248
Miazyn <michelle.ma.fischer@gmail.com>
2024-06-04 17:03:12 +0200

    Added movement to Player and Enemy
    Player and Enemy move both on the same keyboard.
    Player moves via WAD and Enemy via the arrowkeys.
    Issue: Jumping aka 'w' and 'ArrowUp' are currently infinite in this
    version, no jump detection and making them fly off-screen.

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ canvas.height = 576
 
 ctx.fillRect(0, 0, canvas.width, canvas.height)
 
-const gravity = 0.2
+const gravity = 0.3
 
 class Sprite {
     constructor({position, velocity}) {
         this.position = position
         this.velocity = velocity
         this.height = 150
+        this.lastKey
     }
     draw() {
         ctx.fillStyle = 'red'
@@ -21,7 +22,7 @@ class Sprite {
 
     update() {
         this.draw()
-        
+        this.position.x += this.velocity.x
         this.position.y += this.velocity.y
 
         if (this.position.y + this.height + this.velocity.y
@@ -55,6 +56,29 @@ const enemy = new Sprite({
     }
 })
 
+const keys = {
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+    w: {
+        pressed: false
+    },
+    ArrowRight: {
+        pressed: false
+    },
+    ArrowLeft: {
+        pressed: false
+    },
+    ArrowUp: {
+        pressed: false
+    }
+}
+
+let lastKey
+
 function animate() {
     window.requestAnimationFrame(animate)
 
@@ -63,6 +87,76 @@ function animate() {
 
     player.update()
     enemy.update()
+
+    player.velocity.x = 0
+    enemy.velocity.x = 0
+
+    //player Movement
+    if (keys.a.pressed && lastKey === 'a') {
+        player.velocity.x = -1
+    } else if (keys.d.pressed && lastKey === 'd') {
+        player.velocity.x = 1
+    }
+
+    //Enemy Movement
+    if (keys.ArrowLeft.pressed && enemy.lastKey === 'ArrowLeft') {
+        enemy.velocity.x = -1
+    } else if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight') {
+        enemy.velocity.x = 1
+    }
 }
 
 animate()
+
+window.addEventListener('keydown', (event) => {
+    console.log(event.key)
+    switch (event.key) {
+        case 'd':
+            keys.d.pressed = true
+            lastKey = 'd'
+            break
+        case 'a':
+            keys.a.pressed = true
+            lastKey = 'a'
+            break
+        case 'w':
+            player.velocity.y = -10
+            break
+        case 'ArrowRight':
+            keys.ArrowRight.pressed = true
+            enemy.lastKey = 'ArrowRight'
+            break
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed = true
+            enemy.lastKey = 'ArrowLeft'
+            break
+        case 'ArrowUp':
+            enemy.velocity.y = -10
+            break
+        default:
+    }
+})
+
+window.addEventListener('keyup', (event) => {
+    console.log(event.key)
+    switch (event.key) {
+        case 'd':
+            keys.d.pressed = false
+            break
+        case 'a':
+            keys.a.pressed = false
+            break
+        default:
+    }
+
+    //Enemy keys
+    switch (event.key) {
+        case 'ArrowRight':
+            keys.ArrowRight.pressed = false
+            break
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed = false
+            break
+        default:
+    }
+})
